b519897584f85628694f6d4f40cc662d51862555
Lucas da Silva <lucas.dasilva09@hotmail.com>
2025-01-20 22:17:21 -0300

    Config menu

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 var menuIcon = document.querySelector('.menu-icon')
-var ul = document.querySelector('.ul')
+var navList = document.querySelector('.nav_list')
 
 menuIcon.addEventListener('click', ()=>{
-    
-    if (ul.classList.contains('ativo')){
-        ul.classList.remove('ativo');
+    // Mostrar
+    if (navList.classList.contains('ativo')){
+        navList.classList.remove('ativo');
         document.querySelector('.menu-icon img').src = 'img/menu.png'
-    }
-    else{
-        ul.classList.add('ativo');
-        document.querySelector('.menu-icon img').src = 'img/close.png';
-    }
+        document.body.style.overflow = "scroll";
 
+    }
+    // Esconder
+    else{
+        navList.classList.add('ativo');
+        document.querySelector('.menu-icon img').src = 'img/close.png';
+        document.body.style.overflow = "hidden";
+    }
 })
+
+// Ativando e Removendo menu
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    // Ativando link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+    document.body.style.overflow = "scroll";
+
+    // Removendo menu
+    navList.classList.remove('ativo')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))

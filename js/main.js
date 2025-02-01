1f4a0f39ccef196cffdb609a94b5817755a67405
Lucas da Silva <lucas.dasilva09@hotmail.com>
2025-01-31 21:15:20 -0300

    Update project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+    // Menu principal
+
 var menuIcon = document.querySelector('.menu-icon')
 var navList = document.querySelector('.nav_list')
 
@@ -33,3 +35,29 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+    
+    // end Menu Principal
+    // Slider Projetos
+/*
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+// Confirmando primeiro input marcado
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 8000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 2){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
+
+    // end Slider Projetos
+*/
